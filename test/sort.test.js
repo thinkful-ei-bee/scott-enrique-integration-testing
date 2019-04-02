7ba963cfd99bf53542971b7adfaf4bb67f5ee6d0
Enrique Montemayor <eemontemayor@live.com>
2019-04-02 14:12:28 -0500

    finished sort test

--- test/sort.test.js
+++ test/sort.test.js
@@ -1,2 +1,14 @@
 const expect = require('chai').expect;
 const sort = require('../sort.js');
+
+describe('Sort Function', ()=>{
+    it('should return an array of numbers in ascending order', ()=>{
+        expect(sort([6,5,4,3,2,1])).to.deep.equal([1,2,3,4,5,6])
+    })
+    it('should return an array of letters in ascending order', ()=>{
+        expect(sort(['a','c','f','b'])).to.deep.equal(['a','b','c','f'])
+    })
+    it('should sort negative numbers',()=>{
+        expect(sort([-1,-2,-3,-4])).to.deep.equal([-4,-3,-2,-1])
+    })
+})
